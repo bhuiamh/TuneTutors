0ae073ee55c7ddee9f87d0cbb062474fef82947e
bhuiamhe <bhuiamh@gmail.com>
2023-06-12 03:19:26 +0600

    Slider Added

--- src/pages/Slider.jsx
+++ src/pages/Slider.jsx
@@ -1,32 +1,72 @@
-import React from "react";
-import AwesomeSlider from "react-awesome-slider";
-import withAutoplay from "react-awesome-slider/dist/autoplay";
-import "./Slider.css";
-const AutoplaySlider = withAutoplay(AwesomeSlider);
+import React, { useState, useEffect } from "react";
+import LazyLoad from "react-lazy-load";
+
 const Slider = () => {
-  const imageUrls = [
-    "https://drive.google.com/uc?export=view&id=1ciYRvKIykfU8AxXdtpzp-vjKovBwBCWO",
-    "https://drive.google.com/uc?export=view&id=13158CRICaM7vzbQ8IK1t8RcW7QhJRe6O",
-    "https://drive.google.com/uc?export=view&id=1TuFHtfq1aOUmM1Bxb3YtNaMo6-vXwcOj",
-    "https://drive.google.com/uc?export=view&id=1bf9zwE1zKR-QehsRdTfqreofyS0qzHjG",
-    "https://drive.google.com/uc?export=view&id=1H77HQ0deOPj7vPLiAUyfgDM12DvwGdUn",
-    "https://drive.google.com/uc?export=view&id=12_w2oDW6w3RzkjL2lUo46pU2QXND5DIC",
-    "https://drive.google.com/uc?export=view&id=1OG0gL_C4oRzKkOkQrNGzFeMGMnZrM5TP",
-    "https://drive.google.com/uc?export=view&id=14woEpk6_28-SPRkv9NErJ2yVf0uy3vxN",
-    "https://drive.google.com/uc?export=view&id=1u9cxaO97VSvN9ZvnnGagjeizLRpTIaiK",
+  const carouselItems = [
+    "https://drive.google.com/uc?id=1M0z3xlY-CfJw-VZ1PeDaeqwNsqHxYlTZ",
+    "https://drive.google.com/uc?id=1s742No4JEflBi9pGxqDKTq5UqHL3Crp2",
+    "https://drive.google.com/uc?id=1M0z3xlY-CfJw-VZ1PeDaeqwNsqHxYlTZ",
+    "https://drive.google.com/uc?id=1E8Rth6HlwZFXFYON7hte48Bs_bfgfIMd",
+    "https://drive.google.com/uc?id=1zTuKbXncQdLV4E-ogjGc4z7uvYt69VJr",
+    "https://drive.google.com/uc?id=1Lt9joWGRRQIiNtRip1YVDT27EVBXq5KZ",
   ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageLoaded, setImageLoaded] = useState(
+    Array(carouselItems.length).fill(false)
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % carouselItems.length);
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleImageLoad = (index) => {
+    const updatedImageLoaded = [...imageLoaded];
+    updatedImageLoaded[index] = true;
+    setImageLoaded(updatedImageLoaded);
+  };
+
+  const allImagesLoaded = imageLoaded.every((loaded) => loaded);
 
   return (
-    <div>
-      <AutoplaySlider
-        play={true}
-        cancelOnInteraction={false} // should stop playing on user interaction
-        interval={6000}
-      >
-        {imageUrls.map((imageUrl, index) => (
-          <div key={index} data-src={imageUrl} />
-        ))}
-      </AutoplaySlider>
+    <div className="carousel-container m-5">
+      <div className="carousel rounded-lg overflow-hidden">
+        <div
+          className="carousel-inner flex"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
+          {carouselItems.map((item, index) => (
+            <div
+              key={index}
+              className="carousel-item flex-shrink-0 w-full h-96"
+            >
+              {!imageLoaded[index] && (
+                <div className="w-full h-full flex items-center justify-center">
+                  <p>Loading...</p>
+                </div>
+              )}
+              <LazyLoad height={762}>
+                <img
+                  src={item}
+                  className={`w-full h-full object-cover ${
+                    imageLoaded[index] ? "opacity-100" : "opacity-0"
+                  }`}
+                  alt={`Toy ${index + 1}`}
+                  onLoad={() => handleImageLoad(index)}
+                />
+              </LazyLoad>
+            </div>
+          ))}
+        </div>
+      </div>
+      {allImagesLoaded && (
+        <p className="py-4 text-4xl font-bold text-orange-500">
+          Our Special Instrument
+        </p>
+      )}
     </div>
   );
 };
